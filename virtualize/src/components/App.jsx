e681ba61da9716d691acb693f9ee72da55184a85
giladd <giladd@trainologic.com>
2019-09-21 12:40:37 +0300

    do some cleanup

--- virtualize/src/components/App.jsx
+++ virtualize/src/components/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ListContainer from "./ListContainer";
 
 function onRenderCallback(
-  id, // the "id" prop of the Profiler tree that has just committed
-  phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+  _id, // the "id" prop of the Profiler tree that has just committed
+  _phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
   actualDuration, // time spent rendering the committed update
   baseDuration, // estimated time to render the entire subtree without memoization
-  startTime, // when React began rendering this update
-  commitTime, // when React committed this update
-  interactions // the Set of interactions belonging to this update
+  _startTime, // when React began rendering this update
+  _commitTime, // when React committed this update
+  _interactions // the Set of interactions belonging to this update
 ) {
   // Aggregate or log render timings...
   console.log("actualDuration", Math.round(actualDuration));
