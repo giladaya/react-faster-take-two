3e2ebaea8794da7a254b639ec97847ee20ac69d1
giladd <giladd@trainologic.com>
2019-09-22 23:29:53 +0300

    Show render stats

--- virtualize/src/components/App.jsx
+++ virtualize/src/components/App.jsx
@@ -1,24 +1,12 @@
 import React from "react";
 import ListContainer from "./ListContainer";
 
-function onRenderCallback(
-  _id, // the "id" prop of the Profiler tree that has just committed
-  _phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
-  actualDuration, // time spent rendering the committed update
-  baseDuration, // estimated time to render the entire subtree without memoization
-  _startTime, // when React began rendering this update
-  _commitTime, // when React committed this update
-  _interactions // the Set of interactions belonging to this update
-) {
-  // Aggregate or log render timings...
-  console.log("actualDuration", Math.round(actualDuration));
-}
-
-export default function App() {
+export default function App(props) {
+  const {updateStats} = props;
   return (
     <div>
-      <h1>Runtime Demo</h1>
-      <React.Profiler id="PreviewPane" onRender={onRenderCallback}>
+      <h1>App</h1>
+      <React.Profiler id="ListContainer" onRender={updateStats}>
         <ListContainer />
       </React.Profiler>
     </div>
--- virtualize/src/index.js
+++ virtualize/src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function updateStats(
+  id, // the "id" prop of the Profiler tree that has just committed
+  _phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+  actualDuration, // time spent rendering the committed update
+  baseDuration, // estimated time to render the entire subtree without memoization
+  _startTime, // when React began rendering this update
+  _commitTime, // when React committed this update
+  _interactions // the Set of interactions belonging to this update
+) {
+  document.getElementById("stats").innerText = `${id} update: ${actualDuration.toFixed(3)}ms`;
+}
+
+ReactDOM.render(<App updateStats={updateStats} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
