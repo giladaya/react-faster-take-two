71dce1773b332411d6f7c33b65c4bbc0896741c9
giladd <giladd@trainologic.com>
2019-09-25 12:31:33 +0300

    Add keys

--- recon/src/App.js
+++ recon/src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const MAX_LEVELS = 2;
 
 function Counter(props) {
-  const {label, count, setCount} = props;
+  const { label, count, setCount } = props;
   console.log(`Render Counter ${label}`);
   return (
     <div>
-      {label} Count: {count} {' '}
+      {label} Count: {count}{" "}
       <button onClick={() => setCount(count + 1)}>+</button>
     </div>
-  )
+  );
 }
 
 function CountLevel(props) {
-  const {label, count, setCount, level} = props;
+  const { label, count, setCount, level } = props;
   return (
     <>
-      <Counter label={`${label}.`} count={count} setCount={setCount}/>
-      {level < MAX_LEVELS && <Level label={`${label}.`} level={level + 1}/>}
+      <Counter label={`${label}.`} count={count} setCount={setCount} />
+      {level < MAX_LEVELS && <Level label={`${label}.`} level={level + 1} />}
     </>
-  )
+  );
 }
 
 function Level(props) {
-  const {level, label} = props;
+  const { level, label } = props;
   console.log(`Render Level ${label}`);
 
   const [countA, setCountA] = React.useState(0);
@@ -37,11 +37,23 @@ function Level(props) {
   // }
 
   return (
-    <div style={{paddingLeft: `2em`}}>
-      <MCountLevel label={`${label}a`} count={countA} setCount={setCountA} level={level}/>
-      <MCountLevel label={`${label}b`} count={countB} setCount={setCountB} level={level}/>
+    <div style={{ paddingLeft: `2em` }}>
+      <MCountLevel
+        label={`${label}a`}
+        count={countA}
+        setCount={setCountA}
+        level={level}
+        key={`${label}a`}
+      />
+      <MCountLevel
+        label={`${label}b`}
+        count={countB}
+        setCount={setCountB}
+        level={level}
+        key={`${label}b`}
+      />
     </div>
-  )
+  );
 }
 
 const MCountLevel = CountLevel;
@@ -51,7 +63,7 @@ function App() {
   return (
     <div className="App">
       <h1>Reconciliation Demo</h1>
-      <Level level={0} label={''}/>
+      <Level level={0} label={""} />
     </div>
   );
 }
