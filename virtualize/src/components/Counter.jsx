dd8d6ce4369192431bc28e782d2ce24b08222030
giladd <giladd@trainologic.com>
2019-09-25 15:33:57 +0300

    Add minus button

--- virtualize/src/components/Counter.jsx
+++ virtualize/src/components/Counter.jsx
@@ -5,7 +5,8 @@ export default function Counter(props) {
   console.log(`Render Counter ${label}`);
   return (
     <div>
-      {label}: {count} <button onClick={() => setCount(count + 1)}>+</button>
+      {label}: <button onClick={() => setCount(count - 1)}>-</button> {count}{" "}
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
